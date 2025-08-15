182a29a0bdcba50f49404dba9a3efb420f2459ff
Md. Rabiul Islam <rabiul77777888@gmail.com>
2025-08-15 15:48:01 +0600

    comment delete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
+
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
